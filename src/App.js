32fb710da786d158e8b8a0cd88ca81e8769895ea
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-02-10 23:18:45 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import SidebarMenu from './Components/SidebarMenu/SidebarMenu';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='appGlass'>
+     <SidebarMenu></SidebarMenu>
+      </div>
     </div>
   );
 }
---
+++ src/Components/SidebarMenu/SidebarMenu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SidebarMenu = () => {
+    return (
+        <div>
+            <h1>hello</h1>
+        </div>
+    );
+};
+
+export default SidebarMenu;
