15604b0fc7424a6817f7d060f8aad88e07dbe666
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-02-11 00:20:38 +0600

    sidebar done

--- src/Components/SidebarMenu/SidebarMenu.js
+++ src/Components/SidebarMenu/SidebarMenu.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBarsProgress, faAreaChart, faUserFriends, faTasks, faSignOut, faChessQueen } from '@fortawesome/free-solid-svg-icons'
 
 const SidebarMenu = () => {
     return (
         <div>
-            <h1>hello</h1>
+            <h1 className='flex justify-center items-center mt-12 mb-8'>
+                <span className='text-2xl font-bold text-primary-content'><FontAwesomeIcon icon={faChessQueen} /></span>
+                <span className='text-xl ml-4 font-bold text-neutral-focus'>Shapers</span>
+            </h1>
+            <div>
+                <ul class="menu text-neutral-focus font-bold flex justify-center items-start w-56 p-2 rounded-box ml-8 mt-12">
+                    <li>
+                        <a>
+                            <FontAwesomeIcon icon={faBarsProgress} />
+                            Dashboard
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            <FontAwesomeIcon icon={faAreaChart} />
+                            Analytics
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            <FontAwesomeIcon icon={faUserFriends} />
+                            Team
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            <FontAwesomeIcon icon={faTasks} />
+                            Task Reviews
+                        </a>
+                    </li>
+                    <li className='mt-72'>
+                        <a>
+                            Exist
+                            <FontAwesomeIcon icon={faSignOut} />
+                        </a>
+                    </li>
+
+
+                </ul>
+            </div>
+
         </div>
     );
 };
