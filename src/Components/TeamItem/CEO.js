4a8334efdf4c41604a54840107a67d343277fd58
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-05-17 01:46:02 +0600

    ceo done

--- src/Components/TeamItem/CEO.js
+++ src/Components/TeamItem/CEO.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import member from '../../images/pic1.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserFriends, faCheckCircle} from '@fortawesome/free-solid-svg-icons'
+
 
 const CEO = () => {
     return (
-        <div class="hero h-[300px] bg-base-200 m-2 w-[1120px] rounded-xl">
-            <div class="hero-content flex-col lg:flex-row">
-                <img src={member} class="max-w-sm rounded-lg shadow-2xl" />
+        <div class="hero h-[300px] bg-base-200 m-2 w-[1120px] rounded-xl ">
+            <div class="hero-content flex-col lg:flex-row text-info-content">
+                <img className='h-[250px]' src={member} class="max-w-sm rounded-lg shadow-2xl" alt='' />
                 <div>
-                    <h1 class="text-5xl font-bold">Box Office News!</h1>
+                    <h1 class="text-4xl font-bold">MEET THE TEAM</h1>
+                    <br/>
+                    <h1 class="text-2xl  font-bold"><FontAwesomeIcon className='text-primary-focus mr-2' icon={faUserFriends} />Mr. Subrota Biswas</h1>
                     <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     <button class="btn btn-primary">Get Started</button>
                 </div>
