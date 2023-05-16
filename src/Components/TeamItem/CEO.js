22d26ef78b9eddd6e3a48bfe44c2dedf74e7d6b7
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-05-17 02:19:17 +0600

    Team done

--- src/Components/TeamItem/CEO.js
+++ src/Components/TeamItem/CEO.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import member from '../../images/pic1.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUserFriends, faCheckCircle} from '@fortawesome/free-solid-svg-icons'
+import { faUserFriends, faCheckCircle } from '@fortawesome/free-solid-svg-icons'
 
 
 const CEO = () => {
     return (
         <div class="hero h-[300px] bg-base-200 m-2 w-[1120px] rounded-xl ">
             <div class="hero-content flex-col lg:flex-row text-info-content">
-                <img className='h-[250px]' src={member} class="max-w-sm rounded-lg shadow-2xl" alt='' />
+                <img className='h-[250px] w-[600px]' src={member} class="max-w-sm rounded-lg shadow-2xl" alt='' />
                 <div>
                     <h1 class="text-4xl font-bold">MEET THE TEAM</h1>
-                    <br/>
-                    <h1 class="text-2xl  font-bold"><FontAwesomeIcon className='text-primary-focus mr-2' icon={faUserFriends} />Mr. Subrota Biswas</h1>
-                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button class="btn btn-primary">Get Started</button>
+                    <br />
+                    <h1 class="text-2xl  font-bold"><FontAwesomeIcon className='text-primary-focus mr-4' icon={faUserFriends} />Mr. Subrota Biswas</h1>
+                    <br />
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold ' icon={faCheckCircle} />Founder Director</p>
+                    <p>young talented and creative designer with a bulk volume of project management & handover experience with local & multinational companies.</p>
+                    <br />
+                    <a class="btn btn-primary text-primary-content mt-[-10px]" target="_blank" href="https://www.linkedin.com/in/kaniz-mitu/">LinkedIn</a>
                 </div>
             </div>
         </div>
