bc365bd7a7d5b16e5bc2ef1f66b8c3f8fcc01f89
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-05-17 01:09:22 +0600

    css done for members

--- src/Components/TeamItem/OtherMember.js
+++ src/Components/TeamItem/OtherMember.js
@@ -2,67 +2,104 @@ import React from 'react';
 
 const OtherMember = () => {
     return (
-        <div className='grid grid-cols-4 gap-2'>
-            <div class="card card-compact  bg-base-100 shadow-xl">
-                <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+        <div className='grid grid-cols-4 gap-2 m-2'>
+            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
                 <div class="card-body">
-                    <h2 class="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
-                    </div>
+                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
+                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
+                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
+                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
                 </div>
             </div>
-            <div class="card card-compact w-96 bg-base-100 shadow-xl">
-                <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
                 <div class="card-body">
-                    <h2 class="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
-                    </div>
+                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
+                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
+                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
+                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
                 </div>
             </div>
-            <div class="card card-compact w-96 bg-base-100 shadow-xl">
-                <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
                 <div class="card-body">
-                    <h2 class="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
-                    </div>
+                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
+                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
+                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
+                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
                 </div>
             </div>
-            <div class="card card-compact w-96 bg-base-100 shadow-xl">
-                <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
                 <div class="card-body">
-                    <h2 class="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
-                    </div>
+                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
+                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
+                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
+                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
                 </div>
             </div>
-            <div class="card card-compact w-96 bg-base-100 shadow-xl">
-                <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
                 <div class="card-body">
-                    <h2 class="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
-                    </div>
+                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
+                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
+                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
+                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
                 </div>
             </div>
-            <div class="card card-compact w-96 bg-base-100 shadow-xl">
-                <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
                 <div class="card-body">
-                    <h2 class="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
-                    </div>
+                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
+                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
+                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
+                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
                 </div>
             </div>
+            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
+                <div class="card-body">
+                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
+                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
+                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
+                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
+                </div>
+            </div>
+            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
+                <div class="card-body">
+                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
+                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
+                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
+                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
+                </div>
+            </div>
+            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
+                <div class="card-body">
+                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
+                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
+                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
+                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
+                </div>
+            </div>
+            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
+                <div class="card-body">
+                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
+                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
+                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
+                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
+                </div>
+            </div>
+            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
+                <div class="card-body">
+                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
+                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
+                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
+                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
+                </div>
+            </div>
+            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
+                <div class="card-body">
+                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
+                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
+                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
+                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
+                </div>
+            </div>
+            
         </div>
     );
 };
