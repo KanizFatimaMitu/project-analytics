085290abe9cfbe7770abbbe0d8f89bbff8334f68
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-05-17 01:35:51 +0600

    members done

--- src/Components/TeamItem/OtherMember.js
+++ src/Components/TeamItem/OtherMember.js
@@ -1,105 +1,75 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserFriends, faCheckCircle} from '@fortawesome/free-solid-svg-icons'
 
 const OtherMember = () => {
     return (
         <div className='grid grid-cols-4 gap-2 m-2'>
-            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
+            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content">
                 <div class="card-body">
-                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
-                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
-                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
-                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
+                    <h2 class="card-title font-bold"><FontAwesomeIcon className='text-primary-focus' icon={faUserFriends} />SHAHABUDDIN</h2>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold ' icon={faCheckCircle} /><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold' icon={faCheckCircle} /><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold' icon={faCheckCircle} /><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
                 </div>
             </div>
-            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
+            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content">
                 <div class="card-body">
-                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
-                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
-                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
-                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
+                    <h2 class="card-title font-bold"><FontAwesomeIcon className='text-primary-focus' icon={faUserFriends} />FERDOUSHI</h2>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold ' icon={faCheckCircle} /><span className='text-info-content font-bold'>Assistant :</span> Executive</p>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold' icon={faCheckCircle} /><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold' icon={faCheckCircle} /><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
                 </div>
             </div>
-            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
+            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content">
                 <div class="card-body">
-                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
-                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
-                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
-                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
+                    <h2 class="card-title font-bold"><FontAwesomeIcon className='text-primary-focus' icon={faUserFriends} />MD.TOUHIDUR</h2>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold ' icon={faCheckCircle} /><span className='text-info-content font-bold'>Assistant :</span> Accounts Officer </p>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold' icon={faCheckCircle} /><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold' icon={faCheckCircle} /><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
                 </div>
             </div>
-            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
+            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content">
                 <div class="card-body">
-                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
-                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
-                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
-                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
+                    <h2 class="card-title font-bold"><FontAwesomeIcon className='text-primary-focus' icon={faUserFriends} />AL-AMIN</h2>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold ' icon={faCheckCircle} /><span className='text-info-content font-bold'>Assistant :</span> Office Clerk</p>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold' icon={faCheckCircle} /><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold' icon={faCheckCircle} /><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
                 </div>
             </div>
-            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
+            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content">
                 <div class="card-body">
-                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
-                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
-                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
-                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
+                    <h2 class="card-title font-bold"><FontAwesomeIcon className='text-primary-focus' icon={faUserFriends} />PROVAKOR</h2>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold ' icon={faCheckCircle} /><span className='text-info-content font-bold'>Assistant :</span>Project Coordinator</p>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold' icon={faCheckCircle} /><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold' icon={faCheckCircle} /><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
                 </div>
             </div>
-            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
+            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content">
                 <div class="card-body">
-                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
-                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
-                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
-                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
+                    <h2 class="card-title font-bold"><FontAwesomeIcon className='text-primary-focus' icon={faUserFriends} />SAMIMA NITU</h2>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold ' icon={faCheckCircle} /><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold' icon={faCheckCircle} /><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold' icon={faCheckCircle} /><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
                 </div>
             </div>
-            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
+            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content">
                 <div class="card-body">
-                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
-                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
-                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
-                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
+                    <h2 class="card-title font-bold"><FontAwesomeIcon className='text-primary-focus' icon={faUserFriends} />UZZAL HOSSAIN</h2>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold ' icon={faCheckCircle} /><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold' icon={faCheckCircle} /><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold' icon={faCheckCircle} /><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
                 </div>
             </div>
-            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
+            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content">
                 <div class="card-body">
-                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
-                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
-                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
-                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
+                    <h2 class="card-title font-bold"><FontAwesomeIcon className='text-primary-focus' icon={faUserFriends} />FARHANA</h2>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold ' icon={faCheckCircle} /><span className='text-info-content font-bold'>Assistant :</span>Marketing Officer</p>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold' icon={faCheckCircle} /><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
+                    <p><FontAwesomeIcon className='text-primary-focus mr-2 font-bold' icon={faCheckCircle} /><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
                 </div>
             </div>
-            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
-                <div class="card-body">
-                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
-                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
-                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
-                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
-                </div>
-            </div>
-            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
-                <div class="card-body">
-                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
-                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
-                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
-                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
-                </div>
-            </div>
-            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
-                <div class="card-body">
-                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
-                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
-                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
-                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
-                </div>
-            </div>
-            <div class="card card-compact bg-base-100 h-[190px] shadow-xl text-info-content ">
-                <div class="card-body">
-                    <h2 class="card-title font-bold">SAMIMA NITU</h2>
-                    <p><span className='text-info-content font-bold'>Assistant :</span> Architect</p>
-                    <p><span className='text-info-content font-bold'>Graduated from:</span> Dhaka International University.</p>
-                    <p><span className='text-info-content font-bold'>Skill:</span> experience with AutoCAD and 3D studio max.</p>
-                </div>
-            </div>
-            
+           
         </div>
     );
 };
