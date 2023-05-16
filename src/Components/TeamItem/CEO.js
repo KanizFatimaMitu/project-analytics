2e5258a26ab8ede56751bd0c8762d900bcaca64e
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-05-16 23:43:04 +0600

    css changed

---
+++ src/Components/TeamItem/CEO.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import member from '../../images/pic1.jpg'
+
+const CEO = () => {
+    return (
+        <div class="hero h-80 bg-base-200">
+            <div class="hero-content flex-col lg:flex-row">
+                <img src={member} class="max-w-sm rounded-lg shadow-2xl" />
+                <div>
+                    <h1 class="text-5xl font-bold">Box Office News!</h1>
+                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button class="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CEO;
--- src/Pages/Team.js
+++ src/Pages/Team.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import './Shared.css'
+import './Team.css'
 import SidebarMenu from '../Components/SidebarMenu/SidebarMenu';
+import CEO from '../Components/TeamItem/CEO';
 
 const Team = () => {
     return (
-        <div className='appGlass'>
+        <div className='teamGlass'>
             <SidebarMenu></SidebarMenu>
+            <CEO></CEO>
            <h1> here all our team member</h1>
         </div>
     );
