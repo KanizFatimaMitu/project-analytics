c429d8aeea82f340f0ba164f27af4d05ca14e182
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-05-17 19:01:11 +0600

    Reviews done

--- src/Components/Reviews/ReviewsMiddle.js
+++ src/Components/Reviews/ReviewsMiddle.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Review from '../../images/Reviews.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserFriends, faTools, faClock } from '@fortawesome/free-solid-svg-icons'
+
 
 const ReviewsMiddle = () => {
     return (
@@ -7,27 +10,35 @@ const ReviewsMiddle = () => {
             <div>
                 <img className='my-4 rounded-xl' src={Review} alt='' />
             </div>
+
             <div className='grid grid-cols-3 my-4 gap-2'>
                 <div class="card  bg-neutral text-neutral-content">
                     <div class="card-body items-center text-center">
-                        <h2 class="card-title">Cookies!</h2>
+                        <h2 class="card-title"> <FontAwesomeIcon
+                            className=' text-2xl text-primary font-bold mr-2'
+                            icon={faUserFriends} />Great Have Team</h2>
+                        <p>We great communication team.</p>
+                    </div>
+                </div>
+                <div class="card  bg-primary text-primary-content">
+                    <div class="card-body items-center text-center">
+                        <h2 class="card-title"><FontAwesomeIcon
+                        className=' text-2xl font-bold mr-2'
+                        icon={faTools} />Premimum Materials</h2>
                         <p>We are using cookies for no reason.</p>
                     </div>
                 </div>
                 <div class="card  bg-neutral text-neutral-content">
                     <div class="card-body items-center text-center">
-                        <h2 class="card-title">Cookies!</h2>
-                        <p>We are using cookies for no reason.</p>
-                    </div>
-                </div>
-                <div class="card  bg-neutral text-neutral-content">
-                    <div class="card-body items-center text-center">
-                        <h2 class="card-title">Cookies!</h2>
-                        <p>We are using cookies for no reason.</p>
+                        <h2 class="card-title"><FontAwesomeIcon
+                        className=' text-2xl text-primary font-bold mr-2'
+                        icon={faClock} />24/7 delivery <br/> Services</h2>
+                        <p>We provide you 24hr Service all week.</p>
                     </div>
                 </div>
             </div>
         </div>
+
     );
 };
 
--- src/Components/Reviews/ReviewsRight.js
+++ src/Components/Reviews/ReviewsRight.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Review from '../../images/review1.png'
+import Review1 from '../../images/review3.png'
+import Review2 from '../../images/review2.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
@@ -7,29 +9,29 @@ import { faStar } from '@fortawesome/free-solid-svg-icons'
 const ReviewsRight = () => {
     return (
         <div className='grid grid-rows-3 gap-2 m-2'>
-            <div class="card card-side bg-base-100 shadow-xl">
+            <div class="card card-side bg-primary text-primary-content shadow-xl">
 
-                <figure><img className='h-[150px] w-[200px] ' src={Review} alt="Movie" /></figure>
+                <figure><img className='h-[150px] w-[120px] ' src={Review} alt="Movie" /></figure>
                 <div class="card-body">
                     <small className='font-bold text-primary-content'>16 May, 2023</small>
                     <p class="card-title text-sm font-bold ">"shopping with them is great experience !!"</p>
 
-                    <small><FontAwesomeIcon className='text-primary-focus mr-2 font-bold ' icon={faStar} />Click the button to watch on Jetflix app.</small>
+                    <small><FontAwesomeIcon className='text-primary-content mr-2 font-bold ' icon={faStar} />wish you will not be disappointed.</small>
                     <div>
                         <small className='mr-2 font-bold'>87%</small>
-                        <progress class="progress progress-success w-16" value="87" max="100"></progress>
+                        <progress class="progress progress-primary-content w-16" value="87" max="100"></progress>
                     </div>
 
                 </div>
             </div>
-            <div class="card card-side bg-base-100 shadow-xl">
+            <div class="card card-side bg-neutral text-neutral-content shadow-xl">
 
-                <figure><img className='h-[150px] w-[200px] ' src={Review} alt="Movie" /></figure>
+                <figure><img className='h-[150px] w-[120px] ' src={Review1} alt="Movie" /></figure>
                 <div class="card-body">
-                    <small className='font-bold text-primary-content'>16 May, 2023</small>
-                    <p class="card-title text-sm font-bold ">"shopping with them is great experience !!"</p>
+                    <small className='font-bold text-primary-focus'>16 May, 2023</small>
+                    <p class="card-title text-sm font-bold ">"Love their supportive gesture !!"</p>
 
-                    <small><FontAwesomeIcon className='text-primary-focus mr-2 font-bold ' icon={faStar} />Click the button to watch on Jetflix app.</small>
+                    <small><FontAwesomeIcon className='text-primary-focus mr-2 font-bold ' icon={faStar} />watch on Jetflix app.you will be amazed.</small>
                     <div>
                         <small className='mr-2 font-bold'>70%</small>
                         <progress class="progress progress-success w-16" value="70" max="100"></progress>
@@ -37,17 +39,17 @@ const ReviewsRight = () => {
 
                 </div>
             </div>
-            <div class="card card-side bg-base-100 shadow-xl">
+            <div class="card card-side bg-primary text-primary-content shadow-xl">
 
-                <figure><img className='h-[150px] w-[200px] ' src={Review} alt="Movie" /></figure>
+                <figure><img className='h-[150px] w-[120px] ' src={Review2} alt="Movie" /></figure>
                 <div class="card-body">
                     <small className='font-bold text-primary-content'>16 May, 2023</small>
-                    <p class="card-title text-sm font-bold ">"shopping with them is great experience !!"</p>
+                    <p class="card-title text-primary-content text-sm font-bold ">"shopping with them is great experience !!"</p>
 
-                    <small><FontAwesomeIcon className='text-primary-focus mr-2 font-bold ' icon={faStar} />Click the button to watch on Jetflix app.</small>
+                    <small><FontAwesomeIcon className='text-primary-content mr-2 font-bold ' icon={faStar} />Recommanded</small>
                     <div>
                         <small className='mr-2 font-bold'>97%</small>
-                        <progress class="progress progress-success w-16" value="97" max="100"></progress>
+                        <progress class="progress progress-primary-content w-16" value="97" max="100"></progress>
                     </div>
 
                 </div>
